6a15bfb1eb818d61335dd1f1e5eee12483d7db98
Ghazaleh-Graziella <ghazale.f@yahoo.com>
2022-09-16 21:47:57 +0430

    create some tag from styled-component

--- src/components/content.js
+++ src/components/content.js
@@ -3,10 +3,34 @@ import "./style.css";
 
 import styled from "styled-components";
 const Content = () => {
+  const Title = styled.h1`
+    color: #aac9f0;
+  `;
+  const Section = styled.div`
+    background-color: #f0d1aa;
+    display: flex;
+    padding: 5px;
+    margin: 5px;
+    flex-direction: column;
+    justify-content: center;
+    color: #f0aeaa;
+    border-radius: 15px;
+  `;
+
+  const Button = styled.button`
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+    margin: 5px;
+    border-radius: 15px;
+  `;
   return (
-    <div class="section">
-      <h1>Section</h1>
-    </div>
+    <Section class="section">
+      <Title>Section</Title>
+      <button>click me!</button>
+      <button>click me!</button>
+    </Section>
   );
 };
 
