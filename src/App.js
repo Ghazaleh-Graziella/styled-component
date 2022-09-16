b83241584dee26100840753c8f7845b3a7ea55ec
Ghazaleh-Graziella <ghazale.f@yahoo.com>
2022-09-16 21:30:08 +0430

    adding styled-component to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+import Content from "./components/content";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +17,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Content />
       </header>
     </div>
   );
---
+++ src/components/content.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+import styled from "styled-components";
+const Content = () => {
+  return (
+    <div class="section">
+      <h1>Section</h1>
+    </div>
+  );
+};
+
+export default Content;
